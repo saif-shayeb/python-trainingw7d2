eeeb646c81dc7f07f62ffa6302d07f852463dfce
saif-shayeb <153684165+saif-shayeb@users.noreply.github.com>
2026-03-31 18:34:07 +0300

    edit seed.py

--- seed.py
+++ seed.py
@@ -1,11 +1,9 @@
 from app import app, db, Course, Student
 
 with app.app_context():
-    # Clear existing data
     db.drop_all()
     db.create_all()
 
-    # Add courses
     c1 = Course(name='Computer Science')
     c2 = Course(name='Engineering')
     c3 = Course(name='Mathematics')
@@ -14,7 +12,6 @@ with app.app_context():
     db.session.add_all([c1, c2, c3, c4])
     db.session.commit()
 
-    # Add students
     s1 = Student(first_name='John', last_name='Doe', email='john@example.com', course=c1)
     s2 = Student(first_name='Jane', last_name='Smith', email='jane@example.com', course=c2)
     s3 = Student(first_name='Alice', last_name='Johnson', email='alice@example.com', course=c1)
